dda552fde573e11142fc48d9d832cc69d18904fa
Antonio V S Pontes <anvictorsantos@gmail.com>
2022-10-15 14:25:25 -0300

    feature: paginas e rotas

---
+++ pages/servicos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="bg-gray-100">
+            <div class="container mx-auto h-10 flex space-x-4 items-center mb-10">
+                <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
+                <NuxtLink :to="{ name: 'servicos' }">Serviços</NuxtLink>
+            </div>
+        </div>
+        <div>
+            <div class="container mx-auto">
+                <h1 class="text-lg font-bold">Serviços</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, necessitatibus! Autem veritatis
+                    deserunt doloribus alias sequi! Modi rem neque consequuntur odit nobis, porro harum minima omnis,
+                    mollitia fugiat nulla et?</p>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'IndexPage'
+}
+</script>
+  
