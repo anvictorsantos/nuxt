dadd820b0dc3d22997af25905bb7c8d40ee93228
Antonio V S Pontes <developer.antonio.pontes@gmail.com>
2022-10-29 14:35:29 -0300

    aula 08: utilizando fetch

--- pages/servicos.vue
+++ pages/servicos.vue
@@ -5,13 +5,39 @@
         <br>
         <NuxtLink to="/servicos/marketing-digital">Marketing Digital</NuxtLink>
         <br>
+
+        <br>
+        <br>
+
+        <div v-if="$fetchState.pending">
+            Carregando...
+        </div>
+
+        <div v-else>
+            <pre>
+                <div v-for="service in services" :key="service.id">
+                    {{ service.title }}
+                </div>
+            </pre>
+        </div>
+
         <NuxtChild />
     </div>
 </template>
   
 <script>
 export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+
+    data() {
+        return {
+            services: []
+        }
+    },
+
+    async fetch() {
+        this.services = await this.$axios.$get('https://jsonplaceholder.typicode.com/posts')
+    },
 }
 </script>
   
