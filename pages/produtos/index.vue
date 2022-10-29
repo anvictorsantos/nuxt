c222e70a00390bf48343e3c724b47a0228a13c3c
Antonio V S Pontes <developer.antonio.pontes@gmail.com>
2022-10-29 14:22:04 -0300

    aula 07: utilizacao de asynx/await e asyncData

--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -2,6 +2,13 @@
     <div>
         <h1 class="text-lg font-bold">Produtos</h1>
         <NuxtLink to="/produtos/produto-a">Produto A</NuxtLink>
+        <br>
+        <pre>
+            <div v-for="product in products" :key="product.id">
+                {{ product.title }}
+            </div>
+        </pre>
+        <br>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, necessitatibus! Autem veritatis
             deserunt doloribus alias sequi! Modi rem neque consequuntur odit nobis, porro harum minima omnis,
             mollitia fugiat nulla et?</p>
@@ -10,7 +17,21 @@
   
 <script>
 export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+
+    data() {
+        return {
+
+        }
+    },
+    async asyncData({ $axios }) {
+        
+        const products = await $axios.$get('https://jsonplaceholder.typicode.com/posts')
+
+        return {
+            products
+        }
+    },
 }
 </script>
   
